90148bae54ec0105b2a0f2aa7c1994b24c0bca33
tetsuo <info@asano-tetsuo.com>
2025-04-22 15:46:50 +0900

    不要な設定やライブラリを削除

--- src/config/aws-config.sample.ts
+++ src/config/aws-config.sample.ts
@@ -5,9 +5,7 @@ const awsConfig: ResourcesConfig = {
     Cognito: {
       userPoolId: 'your_user_pool_id',
       userPoolClientId: 'your_user_pool_client_id',
-      identityPoolId: 'your_identity_pool_id',
       loginWith: {
-        email: true,
         oauth: {
           domain: 'your_cognito_domain',
           scopes: ['email', 'openid', 'phone'],
@@ -15,23 +13,6 @@ const awsConfig: ResourcesConfig = {
           redirectSignOut: ['http://localhost:5173/'],
           responseType: 'code',
         },
-      },
-      signUpVerificationMethod: 'code',
-      userAttributes: {
-        email: {
-          required: true,
-        },
-        nickname: {
-          required: true,
-        },
-      },
-      allowGuestAccess: true,
-      passwordFormat: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireNumbers: true,
-        requireSpecialCharacters: true,
       },
     },
   },
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import App from './App.vue'
 import router from './router'
 import { Amplify } from 'aws-amplify'
 import awsConfig from '@/config/aws-config.dev'
-import AmplifyVue from '@aws-amplify/ui-vue'
 
 Amplify.configure(awsConfig)
 
@@ -13,6 +12,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(AmplifyVue)
 
 app.mount('#app')
