294d26e1b42495816a917318cf5294860d870096
tetsuo <info@asano-tetsuo.com>
2025-04-23 11:06:08 +0900

    Bootstrap Vue導入

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { createBootstrap } from 'bootstrap-vue-next'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 import App from './App.vue'
 import router from './router'
 import { Amplify } from 'aws-amplify'
@@ -12,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(createBootstrap())
 
 app.mount('#app')
